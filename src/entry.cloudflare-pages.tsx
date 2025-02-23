4a2c912d39afe30891e492796cbabc0822b202e4
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-02-23 21:09:43 +0100

    fix: pnpm linting

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -7,17 +7,10 @@
  * - https://qwik.builder.io/integrations/deployments/cloudflare-pages/
  *
  */
-import {
-  createQwikCity,
-  type PlatformCloudflarePages,
-} from "@builder.io/qwik-city/middleware/cloudflare-pages";
+import { createQwikCity } from "@builder.io/qwik-city/middleware/cloudflare-pages";
 import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
 import render from "./entry.ssr";
-
-declare global {
-  interface QwikCityPlatform extends PlatformCloudflarePages {}
-}
 
 const fetch = createQwikCity({ render, qwikCityPlan, manifest });
 
